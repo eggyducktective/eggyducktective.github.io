2da7909b978526a1a97b8bb44ff859ed70298940
Anna Tran <anhkaehne@gmail.com>
2021-05-08 09:24:09 +1000

    changed console log msg

--- js/main.js
+++ js/main.js
@@ -1,6 +1,3 @@
-console.log('Hello there!!! \n' + 
-'Fancy seeing you here, checking out my portfolio :) \n' +
-'\n' +
-'If you are looking at this, you must be a developer as well :0\n' +
-'Want to give me a call on 0409 060 184 about job opportunities?');
+console.log('WHAT UP \n' + 
+'IM A BIG DUCK!!!');
 
